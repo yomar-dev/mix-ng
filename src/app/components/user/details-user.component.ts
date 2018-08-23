7ad2eacf2f1304761cc810315e28f2bcabbbf13d
Yomar Miranda <mirandayomar@gmail.com>
2018-08-23 00:00:49 -0500

    Obteniendo parametros del padre desde las rutas hijas.

--- src/app/components/user/details-user.component.ts
+++ src/app/components/user/details-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details-user',
@@ -11,7 +12,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) {
+    this.router.parent.params.subscribe(params => {
+      console.log('Ruta hija => ', params);
+    });
+  }
 
   ngOnInit() {
   }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) {
+    this.router.params.subscribe(params => {
+      console.log('Ruta padre => ', params);
+    });
+  }
 
   ngOnInit() {
   }

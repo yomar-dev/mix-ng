802729c4d8897d7c0775b0084f99790363fcfa43
Yomar Miranda <mirandayomar@gmail.com>
2018-08-22 22:57:30 -0500

    ngSwitch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
 import { NgClassComponent } from './components/ng-class/ng-class.component';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgStyleComponent,
     CssComponent,
-    NgClassComponent
+    NgClassComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule
